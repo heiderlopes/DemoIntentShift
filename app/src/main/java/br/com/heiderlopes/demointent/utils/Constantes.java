00262fc9a8ca4851c8ca229a506ab56f37321eb9
logonpf <logonpf@fiap.com.br>
2017-10-21 09:41:05 -0200

    Initial commit

---
+++ app/src/main/java/br/com/heiderlopes/demointent/utils/Constantes.java
@@ -0,0 +1,11 @@
+package br.com.heiderlopes.demointent.utils;
+
+/**
+ * Created by logonpf on 21/10/2017.
+ */
+
+public class Constantes {
+
+    public static String KEY_LOGIN = "KEY_LOGIN";
+    public static String KEY_SENHA = "KEY_SENHA";
+}
